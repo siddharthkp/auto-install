c853c18a8bd4c2b10ba466834169b527d8fd4591
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-08-15 00:17:21 +0530

    Added eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'extends': 'airbnb',
+    'plugins': [
+        'react',
+        'jsx-a11y',
+        'import'
+    ],
+    'rules': {
+        'no-console': 'off',
+        indent: ['error', 4]
+    }
+};
