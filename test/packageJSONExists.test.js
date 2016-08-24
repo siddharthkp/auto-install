d042c4c81cc49a98d1713cf26af9fe3d2b37ba09
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-08-25 03:12:41 +0530

    Added first test

---
+++ test/packageJSONExists.test.js
@@ -0,0 +1,17 @@
+require('chai').should();
+const helpers = require('../src/helpers');
+const syncExec = require('sync-exec');
+
+describe('packageJSONExists', () => {
+    it('should return true', () => {
+        helpers.packageJSONExists().should.equal(true);
+    });
+
+    it('should return false', () => {
+        syncExec('mv package.json package.json.disappeared');
+        helpers.packageJSONExists().should.equal(false);
+        syncExec('mv package.json.disappeared package.json');
+    });
+});
+
+
