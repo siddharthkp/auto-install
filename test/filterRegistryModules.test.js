ed6e3f3ee2357cca9e46cf32b00f3b7bff403618
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-08-25 22:05:07 +0530

    Removed debug statement

--- test/filterRegistryModules.test.js
+++ test/filterRegistryModules.test.js
@@ -5,7 +5,6 @@ const testData = require('./testData.js');
 describe('filterRegistryModules', () => {
     let usedModules = helpers.getUsedModules();
     it('should filter registry modules', () => {
-        console.log(helpers.filterRegistryModules(usedModules));
         helpers.filterRegistryModules(usedModules).should.deep.equal(testData.filteredUsedModules);
     });
 });
