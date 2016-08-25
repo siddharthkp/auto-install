8db3c575adaed3eaae8f0b1e800adfffd2bec468
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-08-25 21:01:47 +0530

    Added test cases for all public helper classes

---
+++ test/filterRegistryModules.test.js
@@ -0,0 +1,12 @@
+require('chai').should();
+const helpers = require('../src/helpers');
+const testData = require('./testdata.js');
+
+describe('filterRegistryModules', () => {
+    let usedModules = helpers.getUsedModules();
+    it('should filter registry modules', () => {
+        console.log(helpers.filterRegistryModules(usedModules));
+        helpers.filterRegistryModules(usedModules).should.deep.equal(testData.filteredUsedModules);
+    });
+});
+
--- test/packageJSONExists.test.js
+++ test/packageJSONExists.test.js
@@ -14,4 +14,3 @@ describe('packageJSONExists', () => {
     });
 });
 
-
